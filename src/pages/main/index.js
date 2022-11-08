7f2dd1fd557b23f418c9c5ee6a270d672db8cf94
Suji Kim <bion239@naver.com>
2022-11-08 22:58:34 +0900

    chore: init

---
+++ src/pages/main/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+function MainPage() {
+  return (
+    <div>
+      <header>
+        <h3>메인 페이지</h3>
+      </header>
+
+      <Button>버튼</Button>
+    </div>
+  );
+}
+
+export default MainPage;
